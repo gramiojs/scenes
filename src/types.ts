25d9237ed565540184cdacf427eee0ce946494f6
kravetsone <57632712+kravetsone@users.noreply.github.com>
2024-11-10 15:55:51 +0300

    fix: type at build

--- src/types.ts
+++ src/types.ts
@@ -4,9 +4,7 @@ export type Modify<Base, Mod> = Omit<Base, keyof Mod> & Mod;
 
 export type StateTypesDefault = Record<string | number, any>;
 
-export type UpdateData<T extends StateTypesDefault> = {
-	__state: T;
-};
+export type UpdateData<T extends StateTypesDefault> = {};
 
 export interface ScenesOptions {
 	storage?: Storage;
